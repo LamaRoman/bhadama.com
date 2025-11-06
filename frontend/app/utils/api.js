76a248f892d3f6df7731fce50bf8227e6dc9f3b9
lamaroman <lamaroman66gmail.com@gmail.com>
2025-11-06 22:54:06 +0545

    feat:add flexible API helper with token support

--- frontend/app/utils/api.js
+++ frontend/app/utils/api.js
@@ -1,16 +1,15 @@
-import { url } from "inspector";
+export async function api(url,options = {}){
+    const token = typeof window !== "undefined"? localStorage.getItem("token"):null;
 
-export const api = async(url, method="GET",body=null)=>{
-    const token = typeof window!=="undefined"? localStorage.getItem("token"):null;
+    const headers = {
+        "Content=Type":"application/json",
+        ...api(options.headers || {}),
+        ...api(token && {Authorization:`Bearer ${token}`}),
+    };
+
+    const res = await fetch(`http://localhost:5001${url}`,{...options,headers});
+    const data = await res.json();
+
+    return data;
+
 }
-
-const headers = {"Content-Type":"application/json"};
-if(token) headers.Authorization = `Bearer ${token}`;
-
-const res = await fetch(`http://localhost:5001${url}`,{
-    method,
-    headers,
-    body:body?JSON.stringyfy(body):null,
-});
-
-return await res.json();
