c7073711fa7a5b4291aadf62de817af85ab5bdff
lamaroman <lamaroman66gmail.com@gmail.com>
2025-11-06 14:01:25 +0545

    Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import 'dotenv/config';
+import cors from "cors"
+
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js"
+
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+
+// Routes
+app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes)
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Hello! Server is working.");
+});
+
+//Testing
+/*
+app.post("/signup", async (req, res) => {
+  const { name, email, password, role } = req.body;
+  res.json({ success: true, received: { name, email, password, role } });
+});
+*/
+
+
+const PORT = process.env.PORT; 
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ backend/src/routes/auth.js
@@ -0,0 +1,52 @@
+import express from "express";
+import {prisma} from "../config/prisma.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+// Test Route
+
+router.get("/",(req,res)=>{
+    res.json({message:"test works"})
+})
+
+// Register
+router.post("/register",async(req,res)=>{
+    const {name,email,password,role} = req.body;
+    try{
+        const existingUser = await prisma.user.findUnique({where:{email}});
+        if (existingUser) return res.status(400).json({error:"Email already exists"});
+    
+        const hashedPassword = await bcrypt.hash(password,10);
+        const user = await prisma.user.create({
+            data:{name,email,password:hashedPassword,role}
+        });
+        const token = jwt.sign({userId:user.id,email:user.email,role:user.role},process.env.JWT_SECRET,{expiresIn:"7d"});
+        res.json({token,user});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({error:"Server error"});
+    }
+});
+
+// Login
+router.post("/login",async(req,res)=>{
+    const {email,password} = req.body;
+    try{
+            const user = await prisma.user.findUnique({where:{email}});
+            if(!user) return res.status(400).json({error:"Invalid credentials"})
+    
+            const isMatch = await bcrypt.compare(password,user.password);
+            if(!isMatch) return res.status(400).json({error:"Invalid credentials"})
+
+            const token = jwt.sign({userId:user.id,email:user.email,role:user.role},process.env.JWT_SECRET,{expiresIn:"7d"});
+            res.json({token,user});
+
+        }catch(err){
+            console.error(err);
+            res.status(500).json({error:"Sercer error"})
+        }
+})
+
+export default router;
