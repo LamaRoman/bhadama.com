5a02097e421608334c92062b68c23bec4c31f598
lamaroman <lamaroman66gmail.com@gmail.com>
2026-01-06 15:24:46 +0545

    a

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,7 +32,7 @@ app.use(cors({
     'http://localhost:3000',
     'https://mybigyard.com',
     'https://www.mybigyard.com',
-    'https://api.mybigyard.com' 
+     
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
